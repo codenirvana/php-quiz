422bb14c802adc5d805fe93ca078d5b35d21ddf5
Udit <codenirvana28@gmail.com>
2015-05-07 20:34:04 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+   <head>
+      <title>Quiz System</title>
+      <link rel="stylesheet" href="css/style.css" media="screen"/>
+   </head>
+   <body>
+      <h2>Quiz System</h2>
+      <?php
+         echo 'using PHP and MSQL';
+      ?>
+   </body>
+</html>
