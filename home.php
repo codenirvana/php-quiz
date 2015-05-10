451ef121fdfcff6774f359686ba75f1e493c6db4
Udit <codenirvana28@gmail.com>
2015-05-10 22:18:11 +0530

    modified home page

--- home.php
+++ home.php
@@ -1,12 +1,19 @@
 <?php
-session_start();
-
-if (isset($_SESSION['username'])){
-   $username = $_SESSION['username'];
-   echo "Hello $username";
-   echo "<a href='logout.php'>Logout</a>";
-} else{
-   echo "You must be login!";
-   echo "<a href='login.php'>Login</a>";
-}
+   session_start();
+   require('inc/header.php');
 ?>
+
+<div class="main">
+   <?php
+      if (isset($_SESSION['username'])){
+         $username = $_SESSION['username'];
+         echo "Hello $username";
+         echo "<a href='logout.php'>Logout</a>";
+      } else{
+         echo "You must be login!";
+         echo "<a href='login.php'>Login</a>";
+      }
+   ?>
+</div>
+
+<?php require('inc/footer.php'); ?>
